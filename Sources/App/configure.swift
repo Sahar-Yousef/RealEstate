35e6d571ef9ae6579a68f16ffed30b5634018e02
Sahar-Yousef <Sell.lla.78@gmail.com>
2024-02-18 11:41:09 +0300

    First Tabale

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,11 +10,11 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        hostname: Environment.get("localhost") ?? "localhost",
+    //    port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("postgres") ?? "postgres",
+        password: Environment.get("") ?? "",
+        database: Environment.get("realestate") ?? "realestate",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
